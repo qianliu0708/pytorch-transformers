62516df4f077b477333c5cc637ecb01ced5d6266
qianliu0708 <qianliu0708@gmail.com>
2019-10-15 16:16:28 +0800

    Update nq_posrank_read_train_short_annotation.py

--- examples/nq_posrank_read_train_short_annotation.py
+++ examples/nq_posrank_read_train_short_annotation.py
@@ -23,6 +23,7 @@ def _open(path):
 
 def read_annotation_for_traingzip(input_files):
     train_annos_short = []
+    print("start:",input_files)
     from tqdm import tqdm
     for input_file in tqdm(input_files):
         with _open(input_file) as input_jsonl:
