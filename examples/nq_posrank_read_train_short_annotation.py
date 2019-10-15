e61c23c31df7e41fcf0ad97c95e4376af7132a74
qianliu0708 <qianliu0708@gmail.com>
2019-10-15 16:07:48 +0800

    10.15

---
+++ examples/nq_posrank_read_train_short_annotation.py
@@ -0,0 +1,93 @@
+import pickle
+import argparse
+import gzip
+import json
+import collections
+import os
+import re
+import glob
+from multiprocessing import cpu_count
+Thread_num = cpu_count()
+import multiprocessing
+from multiprocessing import Pool
+from functools import partial
+import logging
+
+logger = logging.getLogger(__name__)
+def _open(path):
+    if path.endswith(".gz"):
+        return gzip.open(path, "r")
+    else:
+        print("wrong file")
+        exit()
+
+def read_annotation_for_traingzip(input_files):
+    train_annos_short = []
+    from tqdm import tqdm
+    for input_file in tqdm(input_files):
+        with _open(input_file) as input_jsonl:
+
+            for line in input_jsonl:
+                e = json.loads(line, object_pairs_hook=collections.OrderedDict)
+                eid = e["example_id"]
+                anno = e["annotations"][0]
+                question = " ".join(e["question_tokens"])
+                short_ans = anno["short_answers"]
+                if len(short_ans) == 0:
+                    train_annos_short.append([eid,-1, -1, False])
+                else:
+                    answer = short_ans[0]
+                    start_tok = answer["start_token"]
+                    end_tok = answer["end_token"]
+                    if start_tok == -1 or end_tok == -1:
+                        train_annos_short.append( [eid,start_tok, end_tok, False])
+                    else:
+                        train_annos_short.append( [eid,start_tok, end_tok, True])
+    # print("total examples", len(example_short_anno))
+    return train_annos_short
+
+def multiple_read(all_input_files):
+    logger.info('Multiprocessing!')
+    features_initial = []
+    with Pool(Thread_num) as p:
+        annotate = partial(read_annotation_for_traingzip)
+
+        piece_num = int(len(all_input_files) / Thread_num)
+        example_chunks = [all_input_files[start:start + piece_num] for start in range(0, len(all_input_files), piece_num)]
+        logger.info('total chunks: {}'.format(len(example_chunks)))
+
+        for chunk_id, examples_part in enumerate(example_chunks):
+            from tqdm import tqdm
+            features_partial = list(tqdm(p.imap(annotate, examples_part)))
+            features_initial.extend(features_partial)
+            logger.info('processing chunk {}'.format(chunk_id))
+    return features_initial
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_gzip_dir", default=None, type=str, required=True)
+    parser.add_argument("--output_file", default=None, type=str, required=True)
+    args = parser.parse_args()
+    # -------------------------------------generated labels-------------------------------------------
+
+    input_files = []
+    for path in glob.glob("{}/*.gz".format(args.gzip_dir)):
+        input_files.append(path)
+    print("Total gzips:", len(input_files))
+
+    annoted_short_labels = multiple_read(input_files)#[[eid,starttok,endtok,True/False(has answer)]]
+    pickle.dump(annoted_short_labels, open(args.output_file, "wb"))
+    print("Dumpted annotations into {}".format(args.output_file))
+    count_has_short =0
+    count_no_short = 0
+    for e in annoted_short_labels:
+        if e[-1]:
+            count_has_short+=1
+        else:
+            count_no_short+=1
+    print("has short ans:",count_has_short)
+    print("no short ans:",count_no_short)
+
--- examples/nq_posrank_sentences_generation.py
+++ examples/nq_posrank_sentences_generation.py
@@ -322,6 +322,57 @@ def write_nq_predictions(all_examples, all_features, all_results, n_best_size,
 
     return all_nq_predictions,all_nq_nbest_predictions
 #--------------------------lqq end ------------------------------------------------------------
+def convert_predwithsent_cls_format_train(all_pred_with_sent_data,example_labels):
+    '''
+    :param example_labels: a dict{example_id:[start_nq_idx, end_nq_idx, False/True,question]}
+    :param predwithsent_files: a dict {example_id:{generated from nq_posrank_Sentences_generation}}
+    :return:
+    '''
+    all_sents_examples = []
+    count_no_annotation_examples = 0
+    count_has_annotation_examples = 0
+    count_has_answer = 0
+    count_no_answer = 0
+    # data = pickle.load(open(predwithsent_files,"rb"))
+    for (eid,ans_list) in all_pred_with_sent_data.items():
+        question = ans_list[0]["question"]
+        #----------generated distinguished sentens
+        sents_dict = {}
+        if int(eid) in example_labels:
+            label = example_labels[int(eid)]
+            count_has_annotation_examples+=1
+        else:
+            print("an example without annotation")
+            count_no_annotation_examples +=1
+            continue
+
+        for ana in ans_list:
+            if ana["sent"] in sents_dict:
+                continue
+            else:
+                sents_dict[ana["sent"]] = [ana["sent_start_nq_idx"],ana["sent_end_nq_idx"]]
+
+        for (sent,span) in sents_dict.items():
+            if label[-1] and label[0] >= span[0] and label[1] <= span[1]:
+                sent_label = '1'
+                count_has_answer+=1
+            else:
+                sent_label = '0'
+                count_no_answer+=1
+            all_sents_examples.append({
+                'id':int(eid),
+                'question': question,
+                'sentence': sent,
+                'sent_span': span,
+                'label':sent_label,
+            })
+    print("no annotation examples:",count_no_annotation_examples)
+    print("has annotation examples:",count_has_annotation_examples)
+    print("All sents:",len(all_sents_examples))
+    print("\t positive sent:",count_has_answer)
+    print("\t negative sent:",count_no_answer)
+    return all_sents_examples
+
 
 
 if __name__ == '__main__':
@@ -377,7 +428,7 @@ if __name__ == '__main__':
     #
     # # ------------------------------------answer ranker------------------------------------------
     # all_nbest_predictions = pickle.load(open(nbest_pk_file, "rb"))
-    all_nbest_with_sents = {}
+    all_nbest_pred_with_sents = {}
     count_all_sents = 0
     from tqdm import tqdm
     for (eid,nbest_pred) in tqdm(all_nbest_predictions.items()):
@@ -444,11 +495,16 @@ if __name__ == '__main__':
                 if ans["text"] not in ans["sent"]:
                     print("answer: {}, sent: {}".format(ans["text"],ans["sent"]))
 
-        all_nbest_with_sents[eid] = nbest_pred_with_sent
+        all_nbest_pred_with_sents[eid] = nbest_pred_with_sent
         count_all_sents += len(set(sentlist))
 
-    logger.info("Num of examples: %s" % (str(len(all_nbest_with_sents))))
-    logger.info("Averaged sentences for each example: %s" % (str(count_all_sents/len(all_nbest_predictions))))
-    pickle.dump(all_nbest_with_sents,open(nbest_pred_with_sent_file,"wb"))
+    logger.info("Num of examples: %s" % (str(len(all_nbest_pred_with_sents))))
+    logger.info("Averaged distinguished sentences of each example: %s" % (str(count_all_sents/len(all_nbest_predictions))))
+    pickle.dump(all_nbest_pred_with_sents, open(nbest_pred_with_sent_file, "wb"))
     logger.info("Dumped all_nbest_with_sents to : %s" % (nbest_pred_with_sent_file))
+
+    #-----------------------------------from predwithsent to clsformat----------------------
+
+    if args.is_training:
+        convert_predwithsent_cls_format_train(all_nbest_pred_with_sents,example_labels)
 
