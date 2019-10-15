c0b6ac562d89df9547626e9b2cafbdb780630626
qianliu0708 <qianliu0708@gmail.com>
2019-10-15 18:57:33 +0800

    Update gen_posrank_predwithsent.sh

--- gen_posrank_predwithsent.sh
+++ gen_posrank_predwithsent.sh
@@ -2,12 +2,14 @@
 for ((i=0;i<4;i++));do
 {
 python ./examples/nq_posrank_sentences_generation.py \
---example_pk_file /data/nieping/pytorch-transformers/models/wwm_train5piece/examples_${i}_.pk \
---feature_pk_file /data/nieping/pytorch-transformers/models/wwm_train5piece/features_${i}_.pk \
---results_pk_file /data/nieping/pytorch-transformers/models/wwm_train5piece/allresults_${i}_.pk \
---output_nbest_pk_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/train_5_piece/train5piece_nbest_${i}.pk \
---output_pred_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/train_5_piece/train5piece_pred_${i}.json \
---output_nbest_pred_with_sent_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/train_5_piece/train5piece_nbest_predwithsent_${i}.pk
+--example_pk_file /data/nieping/pytorch-transformers/models/wwm_test/examples_${i}_.pk \
+--feature_pk_file /data/nieping/pytorch-transformers/models/wwm_test/features_${i}_.pk \
+--results_pk_file /data/nieping/pytorch-transformers/models/wwm_test/allresults_${i}_.pk \
+--output_nbest_pk_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/dev_all/dev_pred_nbest{i}.pk \
+--output_pred_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/dev_all/dev_pred_${i}.json \
+--output_nbest_pred_with_sent_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/dev_all/dev_nbest_predwithsent_${i}.pk \
+--output_cls_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/dev_all/dev_predwithsent_cls_${i}.json
+
 echo Done ${i}
 } &
 done
